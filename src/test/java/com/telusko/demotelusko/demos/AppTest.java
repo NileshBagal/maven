58e0ed29d5b47efeba5b894c6a7b93704375e48b
Nisha Bagal <nishavb1995@gmail.com>
2020-07-20 22:14:55 +0530

    First commit

---
+++ src/test/java/com/telusko/demotelusko/demos/AppTest.java
@@ -0,0 +1,14 @@
+package com.telusko.demotelusko.demos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+
+	}
+
+}
+
